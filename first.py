78d9fe358f35b4e3397d6eaca7b2c18098d5a608
jewoonlee <2021083518@hanyang.ac.kr>
2023-07-13 14:20:21 +0900

    second

--- first.py
+++ first.py
@@ -1,72 +1,74 @@
-num = int(input())
+# num = int(input())
 
-numlst=[]
-for i in range(1,num+1):
-    num2=int(input())
-    numlst.append(num2)
-
-
-numlst2=list(range(1,num+1))
-numlst3=numlst
+# numlst=[]
+# for i in range(1,num+1):
+#     num2=int(input())
+#     numlst.append(num2)
 
 
-result = []    # 원래 만들 것
-stack= []    #거쳐가는 stack
-re=[]
+# numlst2=list(range(1,num+1))
+# numlst3=numlst
+
+
+# result = []    # 원래 만들 것
+# stack= []    #거쳐가는 stack
+# re=[]
+
+# # while result!=numlst:
+# #     if len(stack) == 0:
+# #         re.append("+")
+# #         stack.append(numlst2[0])
+# #         del numlst2[0]
+    
+# #     else:
+# #         if numlst[0]==stack[len(stack)-1]:
+# #             re.append("-")
+# #             result.append(int(numlst[0]))
+# #             stack.pop()
+# #             del numlst[0]
+# #             if result==numlst:
+# #                 break
+# #             continue
+# #         elif numlst[0]!=stack[len(stack)-1]:
+# #             if len(numlst2)==0:
+# #                 print("No")
+# #                 break
+# #             else:
+# #                 re.append("+")
+# #                 stack.append(numlst2[0])
+# #                 del numlst2[0]
+
 
 # while result!=numlst:
-#     if len(stack) == 0:
-#         re.append("+")
-#         stack.append(numlst2[0])
-#         del numlst2[0]
-    
-#     else:
-#         if numlst[0]==stack[len(stack)-1]:
-#             re.append("-")
-#             result.append(int(numlst[0]))
-#             stack.pop()
-#             del numlst[0]
-#             if result==numlst:
-#                 break
-#             continue
-#         elif numlst[0]!=stack[len(stack)-1]:
+#     if len(stack)!=0:
+#         if numlst[0]!= stack[len(stack)-1]:
 #             if len(numlst2)==0:
-#                 print("No")
+#                 print("NO")
 #                 break
 #             else:
 #                 re.append("+")
 #                 stack.append(numlst2[0])
 #                 del numlst2[0]
+#                 continue
 
-
-while result!=numlst:
-    if len(stack)!=0:
-        if numlst[0]!= stack[len(stack)-1]:
-            if len(numlst2)==0:
-                print("NO")
-                break
-            else:
-                re.append("+")
-                stack.append(numlst2[0])
-                del numlst2[0]
-                continue
-
-        else:
-            re.append("-")
-            result.append(int(numlst[0]))
-            stack.pop()
-            del numlst[0]
-            continue
-    elif len(numlst2)!=0:
-        re.append("+")
-        stack.append(numlst2[0])
-        del numlst2[0]
-    else:
-        break
+#         else:
+#             re.append("-")
+#             result.append(int(numlst[0]))
+#             stack.pop()
+#             del numlst[0]
+#             continue
+#     elif len(numlst2)!=0:
+#         re.append("+")
+#         stack.append(numlst2[0])
+#         del numlst2[0]
+#     else:
+#         break
 
 
 
-if len(result) == num:
-    for i in re:
-        print(i)
+# if len(result) == num:
+#     for i in re:
+#         print(i)
 
+for i in range(10):
+    print (i)
---
+++ second.py
@@ -0,0 +1,39 @@
+n = int(input())
+
+arrayRD=[]
+rearray=[[0 for j in range(100000)] for i in range(n)]
+
+
+for i in range(n):
+    arrayRD = list(input())
+    getn= int(input())
+    getlist = list(input())
+    if arrayRD.count('D')>getn:
+        rearray[i]="error"        
+        continue
+    else:
+        intlist=[]
+        intcount=0
+        for j in range(len(getlist)):
+            if getlist[j].isdigit():
+                intlist.append(int(getlist[j]))
+
+        if arrayRD.count('R')%2!=0:
+            intlist.reverse()
+
+        for k in range(arrayRD.count('D')):
+            del intlist[0]
+
+        rearray[i]=intlist
+
+for j in range(len(rearray)):
+    print(rearray[j])
+
+        
+        
+            
+        
+
+
+
+
