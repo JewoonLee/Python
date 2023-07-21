cafe2e2cbe6c53ea2efe702cbe2cd57c226d94e6
jewoonlee <2021083518@hanyang.ac.kr>
2023-07-21 14:27:35 +0900

    백준 2630

--- 백준/2630.py
+++ 백준/2630.py
@@ -1,71 +1,49 @@
 from sys import stdin
 input = stdin.readline
 
-bluebox=0
+bluebox = 0
 wihte = 0
 
-def boxcount(nlist, nfcol, necol, nfrow, nerow):
+def boxcount(nlist,nfcol,necol,nfrow,nerow,n):
     global bluebox
     global wihte
-    ncol = necol - nfcol + 1
-    nrow = nerow - nfrow + 1
-    ncount = ncol * nrow
+
     countone = 0
     countz = 0
 
-    if ncol <= 0 or nrow <= 0:
+    if necol<=nfcol or nerow<=nfrow:
+        if necol < nfcol or nerow < nfrow:
+            return
+        elif necol == nfcol or nerow == nfrow:
+            if nlist[nfcol][nfrow]==1:
+                bluebox+=1
+            elif nlist[nfcol][nfrow]==0:
+                wihte +=1
         return
-
-    for col in range(nfcol, necol):
-        for row in range(nfrow, nerow):
-            if nlist[col][row] == 1:
-                countone += 1
-            elif nlist[col][row] == 0:
-                countz += 1
-
-    if countone != ncount and countz != ncount:
-        boxcount(nlist, nfcol, int(nfcol + ncol / 2), nfrow, int(nfrow + nrow / 2))
-        boxcount(nlist, int(nfcol + ncol / 2), necol, nfrow, int(nfrow + nrow / 2))
-        boxcount(nlist, nfcol, int(nfcol + ncol / 2), int(nfrow + nrow / 2), nerow)
-        boxcount(nlist, int(nfcol + ncol / 2), necol, int(nfrow + nrow / 2), nerow)
-
-    elif countone == ncount:
-        bluebox += 1
-    elif countz == ncount:
-        wihte += 1
-
-
-# def boxcount(nlist,nfcol,necol,nfrow,nerow):
-#     global bluebox
-#     global wihte
-#     ncol = necol - nfcol + 1
-#     nrow = nerow - nfrow +1
-#     ncount = ncol * nrow 
-#     countone=0
-#     countz=0
         
-#     for col in range(nfcol-1,necol):
-#         for row in range(nfrow-1,nerow):
-#             if nlist[col][row] == 1:
-#                 countone+=1
-#             elif nlist[col][row] == 0:
-#                 countz+=1
+    for col in range(nfcol,necol+1):
+        for row in range(nfrow,nerow+1):
+            if nlist[col][row] == 1:
+                countone+=1
+            elif nlist[col][row] == 0:
+                countz+=1
 
-#     if countone != ncount or countz != ncount :
-#         boxcount(nlist,nfcol,int(necol/2),nfrow,int(nerow/2))
-#         boxcount(nlist,int(necol/2)+1,necol,nfrow,int(nerow/2))
-#         boxcount(nlist,nfcol,int(necol/2),int(nerow/2)+1,nerow)
-#         boxcount(nlist,int(necol/2)+1,necol,int(nerow/2)+1,nerow)
+    if countone != n*n and countz != n*n:
+        boxcount(nlist,nfcol,int(nfcol + n/2)-1,nfrow,int(nfrow + n/2)-1,int(n/2))
+        boxcount(nlist,int(nfcol+ n/2),necol,nfrow,int(nfrow + n/2)-1,int(n/2))
+        boxcount(nlist,nfcol,int(nfcol + n/2)-1,int(nfrow + n/2),nerow,int(n/2))
+        boxcount(nlist,int(nfcol+ n/2),necol,int(nfrow + n/2),nerow,int(n/2))
 
-#     elif nlist.count(1) == ncount:
-#         bluebox+=1
-#     elif nlist.count(0) == ncount:
-#         wihte+=1
-
-
+    elif countone == n * n:
+        bluebox+=1
+        return
+    elif countz == n * n:
+        wihte+=1
+        return
 
 
 n = int(input())
+
 nlist = [[0 for col in range(n)] for row in range(n)]
 
 for i in range(n):
@@ -77,6 +55,8 @@ for i in range(n):
             ncount+=1
 
 
-boxcount(nlist,1,n,1,n)
+
+boxcount(nlist,0,n-1,0,n-1,n)
+
 print(wihte)
 print(bluebox)
